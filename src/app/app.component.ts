ba2f8333e864c5b9a5d115b9c6291da4393deee8
Bruneb4s <105462955+Brunebas@users.noreply.github.com>
2022-11-08 21:51:55 -0300

    jogoDaVelha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jogodavelha';
+
+  x = "https://cdn-icons-png.flaticon.com/512/75/75519.png"
+  o = "https://cdn-icons-png.flaticon.com/512/32/32341.png"
+
+  q1;
+  q2;
+  q3;
+  q4;
+  q5;
+  q6;
+  q7;
+  q8;
+  q9;
+  cont = 0;
+
+  div1(){
+    if(this.q1 == this.x || this.q1 == this.o){
+
+    }else if(this.cont %2 ==0){
+      this.q1 = this.x;
+      this.cont++
+    } else {
+      this.q1 = this.o;
+      this.cont++;
+    }
+  }
 }
+
+
